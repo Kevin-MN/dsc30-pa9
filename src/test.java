1a4a499b93e07c931ffb6be1622910cd106a1d9a
root <kevin.t.moralesnguyen@gmail.com>
2021-12-01 18:57:54 -0800

    asdawd

---
+++ src/test.java
@@ -0,0 +1,18 @@
+import java.sql.Array;
+import java.util.*;
+public class test {
+
+
+    public static void main(String[] args){
+        ContactList test = new ContactList();
+
+
+        ArrayList<String> nums = new ArrayList<String>();
+
+
+
+
+    }
+
+
+}
