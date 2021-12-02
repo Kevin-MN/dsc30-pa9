9ef99e6e5be9f89a8f132a064ce01897997d3e00
root <kevin.t.moralesnguyen@gmail.com>
2021-12-01 18:24:49 -0800

    saondawd

--- src/Person.java
+++ src/Person.java
@@ -35,9 +35,6 @@ public class Person {
     }
 
     public boolean deletePhoneNumber(String pn) {
-        if(this.size == 1){
-            throw new IllegalArgumentException();
-        }
 
         for(int i = 0; i < this.phone_numbers.size();i++){
             if(this.phone_numbers.get(i).compareTo(pn) == 0){
